ef6c348356fd4bf6332f15007b2a0783e0a604e3
boombarash67 <rashidurun@gmail.com>
2021-01-26 19:56:25 +0300

    четвертое задание

---
+++ lesson2/task4.py
@@ -0,0 +1,11 @@
+string = input("введите строку")
+word = []
+num = 1
+for el in range(string.count(' ') + 1):
+   word = string.split()
+if len(str(word)) <= 10:
+        print(f" {num} {word [el]}")
+        num += 1
+else:
+        print(f" {num} {word [el] [0:10]}")
+        num += 1
